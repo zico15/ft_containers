3b95a1fe832cf08c16187925bde382fb25f9a91b
edos-san <zequielzico@gmail.com>
2023-01-22 13:41:17 +0000

    ricardo 2 commit

---
+++ iterator/is_integral.hpp
@@ -0,0 +1,19 @@
+//
+// Created by edos-san on 1/22/23.
+//
+
+#ifndef FT_CONTAINERS_IS_INTEGRAL_HPP
+#define FT_CONTAINERS_IS_INTEGRAL_HPP
+
+namespace ft {
+
+    template< class T >
+    struct is_integral{
+
+    };
+
+
+
+}
+
+#endif //FT_CONTAINERS_IS_INTEGRAL_HPP
